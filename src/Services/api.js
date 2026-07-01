a0fef69b37a3be577b617a0625963961d8f13ce8
irtiza baig <mirzairtiza.dev@gmail.com>
2026-07-01 21:21:00 +0500

    fix

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
+const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
 
 export const api = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
-    headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-        Accept: "application/json",
-    },
-})
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+// IMPORTANT: no Authorization header
